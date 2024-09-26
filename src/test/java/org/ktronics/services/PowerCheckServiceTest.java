7c4ec63b1394d2e5563392aae79e32283909ac29
MahelaDissa <maheladissanayake@gmail.com>
2024-09-26 20:23:29 +0530

    Added intergration test for power service

--- src/test/java/org/ktronics/services/PowerCheckServiceTest.java
+++ src/test/java/org/ktronics/services/PowerCheckServiceTest.java
@@ -1,20 +1,30 @@
 package org.ktronics.services;
 
-import com.microsoft.azure.functions.ExecutionContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.ktronics.models.Credential;
-
-import java.util.Arrays;
 import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
+class PowerCheckServiceIntegrationTest {
 
-public class PowerCheckServiceTest {
+    private PowerCheckService powerCheckService;
+    private Credential credential;
 
+    @BeforeEach
+    void setup() {
+        powerCheckService = new PowerCheckService();
+        credential = new Credential("be2fc4f6-439f-44c3-b663-79a39ea21798", "0777707188", "T98765432", "ShineMonitor");
+    }
+
+    @Test
+    void shouldCheckPowerStationsAndUpdateDatabaseCorrectly() {
+
+        List<String> result = powerCheckService.checkPowerStationsForUser(credential);
+
+        assertNotNull(result, "Result should not be null");
+        assertTrue(result.size() > 0, "Expected some power plants to be processed");
+
+        result.forEach(System.out::println);
+    }
 }
